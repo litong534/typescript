eac8ea194d19480522bbac31be91778a1046edb5
litong <litong@medlinker.com>
2019-04-26 15:25:36 +0800

    feat: deploy-4

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,14 +2,13 @@ module.exports = {
   base: "/typescript/",
   title: "typescript学习记录",
   description: "typescript学习记录",
-  dest: "doc",
+  dest: "docs",
   themeConfig: {
     sidebar: [
       {
         title: "基本类型",
         children: [["/basetype/number/", "数字类型"]]
       }
-    ],
-    docsDir: "docs"
+    ]
   }
 };
